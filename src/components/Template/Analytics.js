2dabc8b655773bea7883d11cab5cb42a15680d50
Luís Cleto <luis@cleto.dev>
2021-05-08 16:15:47 +0200

    Fix React import

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -1,9 +1,14 @@
+import React from 'react';
+
 const { NODE_ENV } = process.env;
 
 const Analytics = () => {
   if (NODE_ENV === 'production') {
-    return <script data-goatcounter="https://cleto.dev.goatcounter.com/count"
-              async src="//gc.zgo.at/count.js"/>;
+    return (<script
+      data-goatcounter="https://cleto.dev.goatcounter.com/count"
+      async
+      src="//gc.zgo.at/count.js"
+    />);
   }
 
   return null;
