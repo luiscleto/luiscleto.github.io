5d9a325a23f166b2fe8f5b163edf68743f5263c6
Luís Cleto <luis@cleto.dev>
2021-05-08 17:07:16 +0200

    Add per-page tracking

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const { NODE_ENV } = process.env;
 
 const Analytics = () => {
+  const { pathname } = useLocation();
+
   if (NODE_ENV === 'production') {
-    return (<script
-      data-goatcounter="https://ksdbfg234jl342lk636l33sd.goatcounter.com/count"
-      async
-      src="//gc.zgo.at/count.js"
-    />);
+    return (
+      <span>
+        <script>{`window.goatcounter = { path: ${pathname} || '/' }`}</script>
+        <script
+          data-goatcounter="https://ksdbfg234jl342lk636l33sd.goatcounter.com/count"
+          async
+          src="//gc.zgo.at/count.js"
+        />
+      </span>);
   }
 
   return null;
