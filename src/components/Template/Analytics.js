e1b9537c71ef1ce4b66cf1ff660c07d822510137
Luís Cleto <luis@cleto.dev>
2021-05-08 16:06:23 +0200

    Configure goatcounter

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -1,13 +1,9 @@
-import { useLocation } from 'react-router-dom';
-
 const { NODE_ENV } = process.env;
 
 const Analytics = () => {
-  const { pathname } = useLocation();
-
   if (NODE_ENV === 'production') {
-    return `<script data-goatcounter="https://${pathname}.goatcounter.com/count"
-              async src="//gc.zgo.at/count.js"/>`;
+    return <script data-goatcounter="https://cleto.dev.goatcounter.com/count"
+              async src="//gc.zgo.at/count.js"/>;
   }
 
   return null;
