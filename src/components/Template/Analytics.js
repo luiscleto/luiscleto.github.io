dce9d8bfb46edf69d4e205b3bf398f4c304019da
Luís Cleto <luis@cleto.dev>
2021-05-08 16:01:36 +0200

    Fix typo

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -1,21 +1,14 @@
-import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const { NODE_ENV } = process.env;
 
-if (NODE_ENV === 'production') {
-  ReactGA.initialize(REACT_APP_GA_TRACKING_ID);
-}
-
 const Analytics = () => {
   const { pathname } = useLocation();
 
-  useEffect(() => {
-    if (NODE_ENV === 'production') {
-      return `<script data-goatcounter="https://${pathname}.goatcounter.com/count"
-              async src="//gc.zgo.at/count.js"/>`
-    }
-  }, [pathname]);
+  if (NODE_ENV === 'production') {
+    return `<script data-goatcounter="https://${pathname}.goatcounter.com/count"
+              async src="//gc.zgo.at/count.js"/>`;
+  }
 
   return null;
 };
