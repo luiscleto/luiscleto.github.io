2f395654599478898e8a196881f5cafcfb96b74c
Luís Cleto <luis@cleto.dev>
2021-10-10 17:25:44 +0200

    Fix twitter link

--- src/data/contact.js
+++ src/data/contact.js
@@ -29,7 +29,7 @@ const data = [
     icon: faInstagram,
   },
   {
-    link: 'https://twitter.com/MisterCacau',
+    link: 'https://twitter.com/luisccleto',
     label: 'Twitter',
     icon: faTwitter,
   },
